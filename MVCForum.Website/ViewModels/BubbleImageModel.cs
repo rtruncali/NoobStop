d7bfba98b3862b104b2d073d049c819bba6e748f
rtruncali <rtruncali@yahoo.com>
2017-02-21 20:07:44 -0800

    Build with MVCForum base

---
+++ MVCForum.Website/ViewModels/BubbleImageModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+using MVCForum.Domain.DomainModel;
+using MVCForum.Domain.DomainModel.Enums;
+using MVCForum.Website.Application;
+
+
+namespace MVCForum.Website.ViewModels
+{
+    public class BubbleImageModel
+    {
+        public Guid BubbleImageID { get; set; }
+
+        [StringLength(150, MinimumLength = 4)]
+        public string Title { get; set; }
+
+        [StringLength(250, MinimumLength = 0)]
+        public string ImageAuthor { get; set; }
+
+        
+    }
+}
